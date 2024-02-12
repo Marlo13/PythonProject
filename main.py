d50cc7fda6274c0a389e77640f97730b617ef748
Marlo <95742923+Marlo13@users.noreply.github.com>
2024-02-12 22:12:35 +0100

    feat(sign-in/up-create-article): add html page create article

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
 import json
+from pprint import pprint
+from typing import Annotated
 from pydantic import BaseModel
+import os
 class LoginForm(BaseModel):
     username: str
     password: str
@@ -16,45 +19,116 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-def read_root():
-    return {"Bye": "Man"}
+# Base de données temporaire pour stocker les articles et les commentaires
+articles_db = {}
+comments_db = {}
+if os.path.exists("articles.json"):
+    with open("articles.json", mode="r", encoding="utf-8") as file:
+        list_articles = json.load(file)
+else:
+    list_articles = []
+
+utilisateurs = {}
+if os.path.exists("users.json"):
+    with open("users.json", mode="r", encoding="utf-8") as file:
+        list_users = json.load(file)
+else:
+    list_users = []
 
 
-@app.get("/template1", response_class=HTMLResponse)
-async def read_template1(request: Request):
 
-    return templates.TemplateResponse(
-        request=request, name="template1.html")
-
-
-def count_occurence(list1):
-    list2 = {}
-    list1 = list1.split(" ")
-    for x in list1:
-        list2[x] = f" apparait {list1.count(x)} fois"
-    formated_list =  list2
-    #formated_list = json.dumps(list2, indent=1)
-    return formated_list
+###### HOME ######
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+     return templates.TemplateResponse("home.html", {"request": request})
 
 
-@app.post("/post_form")
-async def post_form(request: Request, form_data: LoginForm = Depends()):
-        print(form_data)
-        # if form_data["username"] == "admin" and form_data["password"] == "admin":
-        #     return {"Message": "Connexion réussie"}
-        # else:
-        #     return {"Message": "Nom d'utilisateur ou mot de passe incorrect"}z
-    
 
 
-# @app.get("/user_connection")
-# async def user_connection(request: Request, form_data: LoginForm = Depends()):
-#     if form_data["username"] == "admin" and form_data["password"] == "admin":
-#         return {"Message": "Connexion réussie"}
-#     else:
-#         return {"Message": "Nom d'utilisateur ou mot de passe incorrect"}
 
+######## Parti connexion ########
+
+@app.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.post("/login")
+async def login(request: Request, email: Annotated[str, Form()], password: Annotated[str, Form()]):
+    # récupere les données de l'utilisateur de la vue
+    utilisateur = {"email": email, "password": password}
+    #Verification des identifiants
+    with open("users.json", mode="r", encoding="utf-8") as file:
+        users = json.load(file)
+        for user in users:
+            if user["email"] == utilisateur["email"] and user["password"] == utilisateur["password"]:
+                return {"message": "Vous êtes connecté"}
+    raise HTTPException(status_code=400, detail="Mauvais identifiants")
+
+
+
+
+######## Parti Inscription ########
+
+@app.get("/register", response_class=HTMLResponse)
+async def register(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+@app.post("/register")
+async def register(request: Request , email: Annotated[str, Form()], password: Annotated[str, Form()], name: Annotated[str, Form()]):
+    # vérifie dans le fichier json users.json si l'utilisateur existe
+    utilisateur = {"email": email,"password": password,"name": name}
+    for user in list_users:
+        if user["email"] == utilisateur["email"]:
+            raise HTTPException(status_code=400, detail="Cet utilisateur existe déjà")
+    list_users.append(utilisateur)
+    # Ajouter l'utilisateur dans le fichier JSON
+    with open("users.json", mode="w", encoding="utf-8") as file:
+        json.dump(list_users, file, indent=4)
+    return {"message": "Utilisateur créé avec succès"}
+
+
+
+##### Profil #####
+@app.get("/profil", response_class=HTMLResponse)
+async def profil(request: Request):
+    return templates.TemplateResponse("profil.html", {"request": request}, {"list_users": list_users})
+
+
+
+######## Parti articles ########
+# Route pour afficher la liste des articles
+@app.get("/articles", response_class=HTMLResponse)
+async def articles(request: Request):
+    return templates.TemplateResponse("articles.html", {"request": request, "list_articles": list_articles})
+
+# Route pour afficher le formulaire de création d'article
+@app.get("/create_article", response_class=HTMLResponse)
+async def create_article(request: Request):
+    return templates.TemplateResponse("create_article.html", {"request": request})
+
+# Route pour créer un nouvel article
+@app.post("/create_article")
+async def create_article(title: Annotated[str, Form()], content: Annotated[str, Form()]):
+    articles_db = {"title": title, "content": content}
+    list_articles.append(articles_db)
+    # Ajouter l'article dans le fichier JSON
+    with open("articles.json", mode="w", encoding="utf-8") as file:
+        json.dump(list_articles, file, indent=4)
+    return {"message": "Article créé avec succès"}
+
+# editer un article
+@app.get("/edit_article/{article_id}", response_class=HTMLResponse)
+async def edit_article(request: Request, article_id: int):
+    article = list_articles[article_id]
+    return templates.TemplateResponse("edit_article.html", {"request": request, "article": article, "article_id": article_id})
+
+@app.post("/edit_article/{article_id}")
+async def edit_article(request: Request, article_id: int, title: Annotated[str, Form()], content: Annotated[str, Form()]):
+    list_articles[article_id] = {"title": title, "content": content}
+    # Ajouter l'article dans le fichier JSON
+    with open("articles.json", mode="w", encoding="utf-8") as file:
+        json.dump(list_articles, file, indent=4)
+    return {"message": "Article modifié avec succès"}
 
 
 if __name__ == "__main__":
